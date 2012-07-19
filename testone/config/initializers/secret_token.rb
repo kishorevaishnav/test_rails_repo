e6de2d186ce79766f8920ae66005a68c342b25b0
Kishore Kumar Vaishnav <kishorevaishnav@gmail.com>
2012-07-19 14:14:18 +0530

    Initial Commit

---
+++ testone/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testone::Application.config.secret_token = '5a30b4731be83460e3a5dec442598690751434face4c299e4cc150a1e9d6601563599ccf568062748df0413b99e4eeee86ee848b7b16cc6d22b9225ce97cb107'
